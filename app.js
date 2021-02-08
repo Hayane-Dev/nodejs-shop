48927a4eb66898f4e38d51d6e0251fcd841baae2
melkorchi <logimek72@gmail.com>
2021-02-08 23:09:28 +0100

    Routing request

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // process.exit();
-    // console.log(req.url, req.method, req.headers);
-    console.log('url:', req.url);
-    console.log('method', req.method);
-    console.log('headers', req.headers);
+    const url = req.url;
 
-    // Send a response
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>First Page</title></head>');
