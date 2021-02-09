602bccf6bbd9dab9991fb00873e8e4adbbde414d
melkorchi <logimek72@gmail.com>
2021-02-09 02:49:34 +0100

    Handling different routes

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+// All the request url start with '/', so this middleware is alwakys asked
+app.use('/', (req, res, next) => {
+    console.log('This middleware always runs !');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    // We can use res....write...headers...like before but express works for us, he had automatically what we need (ex content-type html...... headers...)
-    res.send('<h1>Hello from Express.js</h1>');
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>Add product page</h1>');
+});
 
+// We have to put it in the end, if not the url /add-product is never reached !
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express.js</h1>');
 });
 
 
