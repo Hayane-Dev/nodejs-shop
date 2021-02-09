9a35d6ac9fbd415c5ec31532072966508226e347
melkorchi <logimek72@gmail.com>
2021-02-09 02:30:01 +0100

    How the middleware works

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('In the middleware')
+    console.log('In the middleware');
     next();
 });
 
 app.use((req, res, next) => {
-    console.log('In another middleware')
-    next();
+    console.log('In another middleware');
+    // We can use res....write...headers...like before but express works for us, he had automatically what we need (ex content-type html...... headers...)
+    res.send('<h1>Hello from Express.js</h1>');
+
 });
 
 const server = http.createServer(app);
