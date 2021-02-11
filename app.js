a88140d811bcd175fff3ead00f1ca54f8b8421eb
melkorchi <logimek72@gmail.com>
2021-02-11 07:51:02 +0100

    Creating html pages

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Filtering paths
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // If no request is intercepted
 app.use((req, res, next) => {
-    // res.send(`
     res.status(404).send(`
         <h1>404 Error: Page not found</h1>
     `);
