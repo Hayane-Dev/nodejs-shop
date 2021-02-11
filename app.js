59af2bb72dcb4ade64bf2cd5722fade6a162980e
melkorchi <logimek72@gmail.com>
2021-02-11 08:27:25 +0100

    Using a helper function for navigation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const path = require('path');
+const rootDir = require('./utils/path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
@@ -15,7 +16,7 @@ app.use(shopRoutes);
 
 // If no request is intercepted
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    res.sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const rootDir = require('../utils/path');
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.get('/', (req, res, next) => {
     // Does not work ... relative path
     // res.sendFile('./views/shop.html');
     // Each argument is a part of the absolute path
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
