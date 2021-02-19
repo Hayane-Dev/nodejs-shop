c4e2ae6a2bfafe4d29eda7d369bf228213399f1b
melkorchi <logimek72@gmail.com>
2021-02-19 22:01:10 +0100

    Syncing Js definitions to the DB

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-const db = require('./utils/database');
+const sequelize = require('./utils/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-// db.execute('SELECT * FROM products')
-//     .then(products => {
-//         console.log('products', products);
-//     })
-//     .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // folder public -> css (make accessible)
@@ -29,6 +23,15 @@ app.use(shopRoutes);
 // If no request is intercepted
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-    console.log("MelShop, Server start on port 3000");
-});
+// Call Sequelize ... Creating tables if needed...there is a check...
+sequelize
+    .sync()
+    .then(result => {
+        console.log(result);
+        app.listen(3000, () => {
+            console.log("MelShop, Server start on port 3000");
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
