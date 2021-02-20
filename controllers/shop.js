334fb682c634514c54ecd075c06213cae958ec40
melkorchi <logimek72@gmail.com>
2021-02-20 19:27:17 +0100

    Refactoring postCart method

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,10 +68,10 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     let fetchedCart; // Pb of scope !!!
+    let newQuantity = 1;
     req.user
         .getCart()
         .then(cart => {
-            // console.log('cart:', cart);
             fetchedCart = cart;
             return cart.getProducts({ where: { id: prodId } }); // Magic method
         })
@@ -80,25 +80,19 @@ exports.postCart = (req, res, next) => {
             if (products.length > 0) {
                 product = products[0];
             }
-            let newQuantity = 1;
+
             if (product) {
                 const oldQuantity = product.cartItem.quantity;
                 newQuantity = oldQuantity + 1;
-                return fetchedCart.addProduct(product, { // Magic method
-                    through: { quantity: newQuantity } // Setting additional fields 
-                });
+                return product;
+
             }
-            return Product
-                .findByPk(prodId)
-                .then(product => {
-                    // console.log('p in cart', product);
-                    return fetchedCart.addProduct(product, { // Magic method
-                        through: { quantity: newQuantity } // Setting additional fields 
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                })
+            return Product.findByPk(prodId)
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, { // Magic method
+                through: { quantity: newQuantity } // Setting additional fields 
+            });
         })
         .then(() => {
             res.redirect('/cart');
