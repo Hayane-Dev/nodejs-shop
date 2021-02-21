6eb5fd8e60ffcb1cb2569327c6f802860123c9d2
melkorchi <logimek72@gmail.com>
2021-02-21 01:37:01 +0100

    Connecting to the MongoDB Server with Mongoose

--- app.js
+++ app.js
@@ -5,15 +5,9 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-const sequelize = require('./utils/database');
+const mongoose = require('mongoose');
 
-// In order to build associations
-const User = require("./models/user");
-const Product = require("./models/product");
-const Cart = require("./models/cart");
-const Order = require("./models/order");
-const CartItem = require("./models/cart-item");
-const OrderItem = require("./models/order-item");
+
 
 const port = 3000;
 const app = express();
@@ -25,18 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // folder public -> css (make accessible)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Storing the user into the request in order to share it
-app.use((req, res, next) => {
-    User
-        .findByPk(1)
-        .then(user => {
-            // Sequelize object (User object...!!!) with all the methods...magic 
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
-});
-
 // Filtering paths
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -44,45 +26,13 @@ app.use(shopRoutes);
 // If no request is intercepted
 app.use(errorController.get404);
 
-// Associations
-// User/Product
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-// User/Cart
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// Cart/Product
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-// Order
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
-
-// Call Sequelize ... Creating tables if needed...there is a check...
-// First lines executed...before the middlewares
-sequelize
-    .sync()
-    .then(() => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Mel', email: 'test@test.com', password: '*****' })
-        }
-        return user;
-    })
-    .then((user) => {
-        // console.log('user:', user);
-        // Creating the cart, magic method
-        return user.createCart();
-    })
-    .then(cart => {
-        // console.log('cart:', cart);
+mongoose
+    .connect('mongodb+srv://mek:mekibnmek@cluster0.akhve.mongodb.net/test')
+    .then(result => {
         app.listen(port, () => {
             console.log("MelShop, Server start on port: " + port + "\n");
         });
     })
     .catch(err => {
         console.log(err);
-    });
+    })
