a91a41c9763fd2b5a9c741b6527286257a027165
melkorchi <logimek72@gmail.com>
2021-02-21 02:36:10 +0100

    Deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -86,10 +86,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-        .then(product => {
-            return product.destroy();
-        })
+    Product.findByIdAndRemove(prodId)
         .then(() => {
             console.log('Destroyed Product');
             res.redirect('/admin/products');
